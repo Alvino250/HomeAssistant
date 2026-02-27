19b77b1253893e4199464d0deb021345783ee4a3
Alvin Grima <grimaalvin@gmail.com>
2026-02-27 13:41:05 +0100

    WakeWord implementation + fixing spotify bugs

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ import threading
 import time
 def main():
     #while(True):
-        #text = transcribe()  # Call the transcribe function to get the text from microphone input
+        text = transcribe()  # Call the transcribe function to get the text from microphone input
         threading.Thread(target=loadModelAsync, daemon=True).start()
-        #text = preProcess(text)  # Preprocess the transcribed text
+        text = preProcess(text)  # Preprocess the transcribed text
         eid, domain, action, parsed = None, None, None, None
         while not ready():
             print("LLM is getting ready...")
             time.sleep(2)
-        text = "Play a song from my playlist Dark/Alt Pop"
+        #text = "Play a song from my playlist Dark/Alt Pop"
         eid, domain, action, parsed = gemma(text)
         if "spotify" in eid or action == "play_media":
             print("Spotify program")
